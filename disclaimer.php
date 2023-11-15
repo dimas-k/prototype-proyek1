3599639785abdd2351b17374659386053177500b
Manday15 <nyshadiningtyas@gmail.com>
2023-11-15 14:05:24 +0700

    penambahan disclaimer

--- disclaimer.php
+++ disclaimer.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="shortcut icon" href="img/polindra21.png">
-    <title>SIKI | Beranda</title>
+    <title>SIKI | Disclaimer</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 mb-5 bg-body-tertiary bg-white">
+<nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 mb-5 bg-body-tertiary bg-white">
         <div class="container">
             <img class="navbar-brand" src="img/polindra2.jpg">
             <span class="position-absolute border border-start border-1 border-black"></span>
@@ -19,7 +19,7 @@
             <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link fs-6 fw-normal font-family-Lucida Sans col-xxl-0 m-0 px-3 py-2" aria-current="page" href="#">Beranda</a>
+                        <a class="nav-link fs-6 fw-normal font-family-Lucida Sans col-xxl-0 m-0 px-3 py-2" aria-current="page" href="index.php">Beranda</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link fs-6 fw-normal font-family-Lucida Sans col-xxl-0 m-0 px-3 py-2" href="#">Paten</a>
@@ -38,7 +38,7 @@
                             Pendaftaraan KI
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item fs-6 fw-normal font-family-Lucida Sans col-xxl-0 m-0 px-3 py-2" href="p-paten.php">Paten</a></li>
+                            <li><a class="dropdown-item fs-6 fw-normal font-family-Lucida Sans col-xxl-0 m-0 px-3 py-2" href="pengajuan.php">Hak Cipta</a></li>
                             <li><a class="dropdown-item fs-6 fw-normal font-family-Lucida Sans col-xxl-0 m-0 px-3 py-2" href="#">Another action</a></li>
                             <li><a class="dropdown-item fs-6 fw-normal font-family-Lucida Sans col-xxl-0 m-0 px-3 py-2" href="#">Something else here</a></li>
                         </ul>
@@ -51,13 +51,20 @@
         </div>
     </nav>
     <div class="container">
-        <img class="img-fluid" src="img/image 1.jpg" id="gambar">
-        <p class="fs-6 fw-normal font-family-Kokoro">
-            <br>
-            <b>Sistem Informasi Kekayaan Intelektual POLINDRA (Sistem Informasi KI POLINDRA)</b> merupakan media informasi perkembangan permohonan KI yang dikelola oleh POLINDRA. Para pemangku kepentingan di POLINDRA, inventor, dan masyarakat luas dapat mengakses dan melihat perkembangan permohonan KI POLINDRA melalui Sistem Informasi KI POLINDRA, baik dalam bentuk grafik, tabel, dan informasi tertulis lainnya. Sistem Informasi KI POLINDRA menyediakan fitur-fitur perkembangan proses permohonan pelindungan KI POLINDRA, abstrak atau deskripsi singkat setiap KI, unduh berkas KI, dan formulir pengajuan KI online melalui unit pengelola KI POLINDRA.
-        </p>
-
+        <p class="fs-3 fw-bold font-family-Khula col-xxl-1 m-0 px-3 py-2">Disclaimer</p>
+        <br>
+        <ul>
+            <li class="fs-6 fw-normal font-family-Kokoro">Disclaimer atau singkatan ini berguna bagi kami sebagai perlindungan terhadap tuntutan hukum dari pihak-pihak yang kurang bertanggung jawab.</li>
+            <li class="fs-6 fw-normal font-family-Kokoro">Semua informasi yang dimuat dalam Sistem Informasi Kekayaan Intelektual Institut Pertanian Bogor (Sistem Informasi KI IPB) sudah diusahakan seakurat mungkin. Walau demikian, tidak menutup kemungkinan adanya kesalahan yang tidak disengaja baik oleh administrator maupun karena kesalahan sistem.</li>
+            <li class="fs-6 fw-normal font-family-Kokoro">Susunan (urutan) nama-nama inventor pada Sistem Informasi KI IPB kemungkinan tidak sesuai karena kendala teknis pada sistem. Susunan (urutan) nama-nama inventor yang benar terdapat pada Formulir Permohonan Pendaftaran Paten yang jika diperlukan dapat menghubungi Direktorat Inovasi dan Kekayaan Intelektual IPB.</li>
+            <li class="fs-6 fw-normal font-family-Kokoro">Gunakan situs ini sebagai referensi pengetahuan, bukan sebagai rujukan absolut/mutlak.</li>
+            <li class="fs-6 fw-normal font-family-Kokoro">Dengan menggunakan situs ini, Anda mengakui dan menyetujui bahwa IPB tidak bertanggung jawab dan tidak dapat disalahkan ataupun dituntut atas hasil apapun yang terjadi sebagai akibat dari penggunaan situs ini.</li>
+        </ul>
+        <br><br>
+        <span class="d-flex justify-content-center">
+            <img src="img/imgdis.png">
+        </span>
     </div>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="aset/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
